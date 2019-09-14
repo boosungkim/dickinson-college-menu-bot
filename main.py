11757a851e6331577ff172dfcfc6d50efabbb458
boosungkim <boosungk@gmail.com>
2019-09-14 10:47:11 -0400

    V0.9, organized code+formatted the email to replace certain characters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datetime
-import schedule
+# import schedule
 import requests
 from bs4 import BeautifulSoup
 <<<<<<< HEAD
@@ -58,15 +58,15 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from configparser import ConfigParser
+import re
 
 #CHANGE TO A SOLID TIMEZONE TIME
-#CHANGE /W to WITH
 
-#check date
 def getLink():
     currentDatetimeRaw = datetime.datetime.today()
     currentDatetime = str(currentDatetimeRaw)
     
+    todayDate = currentDatetime[:10]
     yr = currentDatetime[2:4]
     mo = currentDatetime[5:7]
     if mo[0] == "0":
@@ -76,12 +76,12 @@ def getLink():
         da = currentDatetime[9]
     #use date to get website number
     link = "https://www.dickinson.edu/site/custom_scripts/dc_dining_menus_index.php?yr={}&mo={}&da={}".format(yr,mo,da)
-    return link
+    return [link, todayDate]
 
 
-def getMenu(link):
+def getMenu(a_link):
     #Grab data
-    page = requests.get(link)
+    page = requests.get(a_link)
     soup = BeautifulSoup(page.content, 'html.parser')
     #convert list elements into string
     overallRaw = soup.select("div p")
@@ -116,76 +116,43 @@ def getMenu(link):
 
 def formatMenu(stringList):
     formattedMenu = ""
-    for a in stringList:
-        if a == "Breakfast":
+    for ln in stringList:
+        if ln == "Breakfast":
             formattedMenu = "-Breakfast-"
-        elif a == "Lunch":
+        elif ln == "Lunch":
             formattedMenu = "-Lunch-"
-        elif a == "Dinner":
+        elif ln == "Dinner":
             formattedMenu = "-Dinner-"
         else:
-            formattedMenu = formattedMenu + "\n {}".format(a)
+            f_ln = re.compile(ln)
+            f_ln = re.sub(":", ": ", ln)
+            f_ln_2 = re.sub("w/", "with ", f_ln)
+            f_ln_3 = re.sub("&", "and", f_ln_2)
+            formattedMenu = formattedMenu + "\n {}".format(f_ln_3)
     return formattedMenu
-
-
-# def sendMenu(breakfast, lunch, dinner):
-#     parser = ConfigParser()
-#     parser.read('config.ini')
-#     #Getting info to log in
-#     myEmail = parser.get('SensitiveData', 'myEmail')
-#     myPassword = parser.get('SensitiveData', 'myPassword')
-#     hostName = parser.get('SensitiveData', 'hostName')
-#     smtpCode = parser.get('SensitiveData', 'smtpCode')
-#     subject = "Today's Menu"
-#     #ADD DATE HERE IN FUTURE
-#     message = "Hello, I am Boo Sung Kim's Dickinson Menu Bot. Today's menu is: \n \n" + breakfast + "\n \n" + lunch + "\n \n" + dinner
-    
-#     #Getting emails
-#     with open('email.txt', 'r') as f:
-#         recipientEmail = f.readline()
-
-    # msg = MIMEMultipart()
-    # msg['From'] = myEmail
-    # msg['To'] = recipientEmail
-    # msg['Subject'] = subject
-    # msg.attach(MIMEText(message, 'plain'))
-    # server = smtplib.SMTP()
-    # server.connect('127.0.0.1', smtpCode)
-    # server.starttls()
-    # # server.ehlo()
-    # server.login(myEmail, myPassword)
-    # text = msg.as_string()
-    # server.sendmail(myEmail, recipientEmail, text)
-    # with open('email.txt', 'r') as f:
-    #     for recipientEmail in f:
-    #         server.sendmail(myEmail, recipientEmail, text)
-    # server.quit()
 
 
 def main():
     menuLink = getLink()
-    threeMeals = getMenu(menuLink)
+    threeMeals = getMenu(menuLink[0])
     breakfast = formatMenu(threeMeals[0])
     lunch = formatMenu(threeMeals[1])
     dinner = formatMenu(threeMeals[2])
-    message = "Hello, I am Boo Sung Kim's Dickinson Menu Bot. Today's menu is: \n \n" + breakfast + "\n \n" + lunch + "\n \n" + dinner
-    # sendMenu(breakfast, lunch, dinner)
-
+    message = "Hello, I am Boo Sung Kim's Dickinson Menu Bot V0.9.\n" + "My GitHub link: https://github.com/boosungkim/Dickinson_Menu_Bot\n\n" + "Today's menu is: \n \n" + breakfast + "\n \n" + lunch + "\n \n" + dinner + "\n \n \n" + "Have a nice day!"
     parser = ConfigParser()
     parser.read('config.ini')
-    #Getting info to log in
+    # Getting info to log in
     myEmail = str(parser.get('SensitiveData', 'myEmail'))
     myPassword = str(parser.get('SensitiveData', 'myPassword'))
     hostName = str(parser.get('SensitiveData', 'hostName'))
     smtpCode = int(parser.get('SensitiveData', 'smtpCode'))
-    subject = "Today's Menu"
+    subject = "{} Dickinson Dining Menu".format(menuLink[1])
 
     recipientEmail = []
     with open('email.txt', 'r') as f:
         for line in f:
             line = line.strip()
             recipientEmail.append(str(line))
-    #ADD DATE HERE IN FUTURE
     subject = "Today's Menu"
     msg = MIMEMultipart()
     msg['From'] = myEmail
