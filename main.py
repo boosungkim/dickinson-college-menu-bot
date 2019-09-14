9bd7ecb88a3a4cb71ae7866bd962470125d49649
boosungkim <boosungk@gmail.com>
2019-09-14 12:30:08 -0400

    V0.9.1

--- main.py
+++ main.py
@@ -2,58 +2,6 @@ import datetime
 # import schedule
 import requests
 from bs4 import BeautifulSoup
-<<<<<<< HEAD
-"""
-Sunday 9/8: https://www.dickinson.edu/site/custom_scripts/dc_dining_menus_index.php
-Monday 9/9: https://www.dickinson.edu/site/custom_scripts/dc_dining_menus_index.php?yr=19&mo=9&da=9
-Tuesday 9/10: https://www.dickinson.edu/site/custom_scripts/dc_dining_menus_index.php?yr=19&mo=9&da=10
-"""
-
-#CHANGE TO A SOLID TIMEZONE TIME
-#check date
-currentDatetimeRaw = datetime.datetime.today()
-currentDatetime = str(currentDatetimeRaw)
-
-yr = currentDatetime[2:4]
-mo = currentDatetime[5:7]
-if mo[0] == "0":
-    mo = currentDatetime[6]
-da = currentDatetime[8:10]
-if da[0] == "0":
-    da = currentDatetime[9]
-
-#use date to get website number
-link = "https://www.dickinson.edu/site/custom_scripts/dc_dining_menus_index.php?yr={}&mo={}&da={}".format(yr,mo,da)
-
-#Grab data
-page = requests.get(link)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-#convert list elements into string
-overallRaw = soup.select("div p")
-overallStr = [str(i) for i in overallRaw]
-
-#Isolate Dining Hall menu
-indices = [i for i, x in enumerate(overallStr) if x == '<p><span style="color:#666;">Lunch</span></p>']
-toRemove = indices[1]
-isolatedMenu = overallStr[1:toRemove]
-
-#Organizing the isolated menu
-bf= isolatedMenu[0].replace('<p><span style="color:#666;">', "").replace('</span></p>', "")
-print(bf)
-
-
-
-#Lunch
-
-#Dinner
-
-
-#grab the text
-#have a phone number
-#text the menu
-#create schedule
-=======
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -170,4 +118,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> 4a3badd... Organized code, now has separate config and email list files
