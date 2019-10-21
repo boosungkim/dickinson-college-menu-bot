54794eb35c37cbf68faca2b4c61bacf94da0c63d
boosungkim <boosungk@protonmail.com>
2019-10-21 01:53:32 -0400

    Additional changes

--- testing.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class testing{
-    public static void main(String[] args){
-        String html = "<html><head><title>First parse</title></head>"
-  + "<body><p>Parsed HTML into a doc.</p></body></html>";
-Document doc = Jsoup.parse(html);
-    }
-}
